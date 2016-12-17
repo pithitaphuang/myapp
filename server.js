14280bd42c5b8dd2024cd3b5cc8cebc3531a1ef1
pithita <pithita_phuang@kkumail.com>
2016-12-17 11:56:33 +0700

    change2

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ router.route('/getDataDate')
     .post(function(req, res) {
 
     	var db = admin.database();
-var ref = db.ref("project/UserData/" + req.body.UID + "/" + "Subject/" + req.body.Subject;
+var ref = db.ref("project/UserData/" + req.body.UID + "/" + "Subject/" + req.body.Subject);
 
 console.log("Firebase started ~~~~~~~~");
 
